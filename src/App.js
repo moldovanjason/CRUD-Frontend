d35569c7fd8c95714760499f1af6d0372560428f
Jason Moldovan <moldovanjason@gmail.com>
2021-08-05 17:13:15 -0400

    done for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([]);
+  const [newReview, setNewReview] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/get").then((response) => {
+      setMovieList(response.data);
+    });
+  }, []);
+
+  const submitReview = () => {
+    axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    });
+
+    setMovieList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: review },
+    ]);
+  };
+
+  const deleteReview = (movie) => {
+    axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  };
+
+  const updateReivew = (movie) => {
+    axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+          placeholder="Movie Name..."
+        />
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+          placeholder="Movie Review..."
+        />
+        <button className="postBtn" onClick={submitReview}>
+          Post Review
+        </button>
+        {movieReviewList.map((val) => {
+          return (
+            <div className="card">
+              <h1>{val.movieName}</h1>
+              <p>{val.movieReview}</p>
+
+              <button
+                className="deleteBtn"
+                onClick={() => {
+                  deleteReview(val.movieName);
+                }}
+              >
+                Delete
+              </button>
+              <input
+                className="editInput"
+                type="text"
+                id="updateInput"
+                placeholder="Add New Review..."
+                onChange={(e) => setNewReview(e.target.value)}
+              />
+              <button
+                className="updateBtn"
+                onClick={() => {
+                  updateReivew(val.movieName);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
